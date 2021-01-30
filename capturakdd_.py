263cc3ca356bca64018a9d8d2e73f758caa7f996
GAYA ANALYTICS <jayronsoares@gmail.com>
2021-01-30 18:00:14 -0300

    Update capturakdd_.py

--- capturakdd_.py
+++ capturakdd_.py
@@ -1,5 +1,5 @@
 '''
-Este algoritmo usado minerar tweets no Twitter.com de acordo com os rótulos de interesses.
+Algoritmo usado minerar tweets no Twitter.com de acordo com os rótulos de interesses.
 '''
 from __future__ import absolute_import, print_function
 import tweepy
